91d4fa64ecd24168af01af1101e100f64ca4bc38
Mapi <mapitenar@hotmail.com>
2024-04-22 19:17:59 +0200

    Implemented concurrency using a tabpane

--- src/main/java/com/svalero/pokedexreactive/controller/AppController.java
+++ src/main/java/com/svalero/pokedexreactive/controller/AppController.java
@@ -3,67 +3,39 @@ package com.svalero.pokedexreactive.controller;
 import java.util.ResourceBundle;
 
 import com.svalero.pokedexreactive.Utils.Constants;
-import com.svalero.pokedexreactive.Utils.SpriteCell;
-import com.svalero.pokedexreactive.model.Pokemon.PokemonInfo;
-import com.svalero.pokedexreactive.task.PokemonTask;
-
+import java.io.IOException;
 import java.net.URL;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 
 public class AppController implements Initializable {
     // Match of the components of the user interface with the controller.
-    private ObservableList<PokemonInfo> pokemons;
     @FXML
     private ComboBox<String> pokeTypeComboBox;
     @FXML
     private Button buttonSearch;
     @FXML
-    private TableView<PokemonInfo> pokeTableView;
-    @FXML
-    private TableColumn<PokemonInfo, String> frontColumn;
-    @FXML
-    private TableColumn<PokemonInfo, String> nameColumn;
-    @FXML
-    private TableColumn<PokemonInfo, String> numberColumn;
-    @FXML
-    private TableColumn<PokemonInfo, String> abilitiesColumn;
-    @FXML
-    private TableColumn<PokemonInfo, String> backColumn;
-
-    /*@FXML
-    private TabPane tabPaneResults;*/
-    private PokemonTask pokemonTask;
+    private TabPane tabPaneResults;
 
     @Override
     public void initialize (URL location, ResourceBundle resources) {
         //I load the list of constants into the combobox
         ObservableList<String> options = FXCollections.observableArrayList(Constants.pokeTypes);
         pokeTypeComboBox.setItems(options);
-        //
-        frontColumn.setCellValueFactory(new PropertyValueFactory<>("frontSprite"));
-        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-        numberColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-        abilitiesColumn.setCellValueFactory(new PropertyValueFactory<>("abilitiesString"));
-        backColumn.setCellValueFactory(new PropertyValueFactory<>("backSprite"));
-        frontColumn.setCellFactory(col -> new SpriteCell<>());
-        backColumn.setCellFactory(col -> new SpriteCell<>());
-        //tabPaneResults.setTabClosingPolicy(TabPane.TabClosingPolicy.ALL_TABS);
+        tabPaneResults.setTabClosingPolicy(TabPane.TabClosingPolicy.ALL_TABS);
     }
 
-    @FXML
+    /*@FXML
     public void searchPokemon(ActionEvent event){
         this.pokemons = FXCollections.observableArrayList();
         System.out.println("Inicio");
@@ -73,6 +45,22 @@ public class AppController implements Initializable {
         System.out.println("Busco " + requestedType);
         pokemonTask = new PokemonTask(requestedType, this.pokemons);
         new Thread(pokemonTask).start();
-    }
+    }*/
+
+    @FXML 
+    public void searchPokemon(ActionEvent event){
+        
+        String requestedType = pokeTypeComboBox.getValue().toString();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/pokeWindow.fxml"));
+        PokemonController pokemonController = new PokemonController(requestedType);
+        loader.setController(pokemonController);
+        try {
+            AnchorPane pokePane = loader.load();
+            tabPaneResults.getTabs().add(new Tab(requestedType, pokePane));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    } 
 
 }
---
+++ src/main/java/com/svalero/pokedexreactive/controller/PokemonController.java
@@ -0,0 +1,63 @@
+package com.svalero.pokedexreactive.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.svalero.pokedexreactive.Utils.SpriteCell;
+import com.svalero.pokedexreactive.model.Pokemon.PokemonInfo;
+import com.svalero.pokedexreactive.task.PokemonTask;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class PokemonController implements Initializable {
+
+    @FXML
+    private TableView<PokemonInfo> pokeTableView;
+    @FXML
+    private TableColumn<PokemonInfo, String> frontColumn;
+    @FXML
+    private TableColumn<PokemonInfo, String> nameColumn;
+    @FXML
+    private TableColumn<PokemonInfo, String> numberColumn;
+    @FXML
+    private TableColumn<PokemonInfo, String> abilitiesColumn;
+    @FXML
+    private TableColumn<PokemonInfo, String> backColumn;
+    
+    private ObservableList<PokemonInfo> pokemons;
+    private String requestedType;
+    private PokemonTask pokemonTask;
+
+
+    public PokemonController (String requestedType) {
+        this.requestedType = requestedType;
+    }
+
+    @Override
+    public void initialize (URL location, ResourceBundle resources) {
+        //I give value to the columns of the tableview
+        frontColumn.setCellValueFactory(new PropertyValueFactory<>("frontSprite"));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        numberColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        abilitiesColumn.setCellValueFactory(new PropertyValueFactory<>("abilitiesString"));
+        backColumn.setCellValueFactory(new PropertyValueFactory<>("backSprite"));
+        frontColumn.setCellFactory(col -> new SpriteCell<>());
+        backColumn.setCellFactory(col -> new SpriteCell<>());
+        
+        this.pokemons = FXCollections.observableArrayList();
+        System.out.println("Inicio");
+        this.pokeTableView.setItems(this.pokemons); //Binds the list observable to the listview
+        
+        System.out.println("Busco " + requestedType);
+        pokemonTask = new PokemonTask(requestedType, this.pokemons);
+        new Thread(pokemonTask).start();
+
+    }
+
+}
